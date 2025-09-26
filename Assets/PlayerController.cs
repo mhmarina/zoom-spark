a5fa297414ec8220156e4e4f76ada6cb9b6fc72d
Marina <140115877+mhmarina@users.noreply.github.com>
2025-09-26 10:08:45 -0700

    Player movement, camera follow

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float mv = moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector2(transform.position.x + mv, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector2(transform.position.x - mv, transform.position.y);
+        }
+        if (isGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            // jump
+            rb.AddForceY(jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return rb.linearVelocityY >= 0;
+    }
+}
