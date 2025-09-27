6f78bb703a35c4e29bf5281512d6e462b6a2c40e
Marina <140115877+mhmarina@users.noreply.github.com>
2025-09-27 13:56:30 -0700

    Camera follow cursor

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,24 +3,39 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject Player;
-    [SerializeField] float followDelay;
-    [SerializeField] float followSpeed;
-    
+    [SerializeField] float followDelay = 1.0f;
+    [SerializeField] float followSpeed = 5.0f;
+    [SerializeField] float cursorFollowSpeed = 1.0f;
+
     private Transform playerTransform;
 
     void Start()
     {
-        playerTransform = Player.transform;
+        if(Player != null)
+        {
+            playerTransform = Player.transform;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float distance = Vector2.Distance(transform.position, playerTransform.position);
-        if(distance >= followDelay)
+        Vector3 targetPos;
+
+        if (Player != null && Player.activeInHierarchy)
         {
-            Vector3 newPos = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
+            float distance = Vector2.Distance(transform.position, playerTransform.position);
+            if (distance >= followDelay)
+            {
+                targetPos = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+                transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+            }
+        }
+        // follow cursor if player is inactive
+        else
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos = new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPos, cursorFollowSpeed * Time.deltaTime);
         }
     }
 }
