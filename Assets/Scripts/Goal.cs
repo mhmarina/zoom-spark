5a3c0613304263edbc2b044e390335ef94a93c46
Marina <140115877+mhmarina@users.noreply.github.com>
2025-09-28 01:33:26 -0700

    Minor Bug Fix 2

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -24,6 +24,7 @@ public class Goal : MonoBehaviour, IInteractable
         if (ns == null) return;
 
         string currName = ns.name;
+        Debug.Log($"{currName}, {target}");
         if (currName == target)
         {
             onGoalAchieved();
